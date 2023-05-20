8b608e097faed8f99e70fc18051d9f66eef374b8
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-05-20 12:28:41 -0300

    add page home and styles global

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { Posts } from './components/Posts'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Posts />
+      <Footer />
+    </>
+  )
+}
